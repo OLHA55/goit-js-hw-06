d8a2e054133d9be0bd2ff9e2a98a3fc01c280cb5
OLHA55 <bagira2310@gmail.com>
2024-03-02 11:43:23 +0200

    js06new002

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,31 @@
 
+class StringBuilder {
+    constructor(value) {
+      this.value = value;
+    }
+
+    get val() {
+      return this.value;
+    }
+
+    set val(value) {
+      this.value = value;
+    }
+
+    append(str) {
+      this.value += str;
+    }
+
+    prepend(str) {
+      this.value = str + this.value;
+    }
+    // додає елемент на поч і в кін
+    pad(str) {
+      this.value = str + this.value + str;
+    }
+  }
+
+
 
 
 
