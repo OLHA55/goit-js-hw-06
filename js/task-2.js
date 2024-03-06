54bb6041216f3e60f07fa1b4d7b3bfb861c826a4
OLHA55 <bagira2310@gmail.com>
2024-03-06 14:55:45 +0200

    js06new008

--- js/task-2.js
+++ js/task-2.js
@@ -1,37 +1,39 @@
 const storage = ["Nanitoids", "Prolonger","Antigravitator"];
 class Storage {
     constructor() {
-        this.items = `products`;
+        this.items = items;
     }
 }
 
-    getProducts() {
-        const productsstorage = storage.getItems(this.items);
-        if (productsstorage !== null) {
-            return JSON.parse(products.Storage);
-        }
-        return [];
-    }
+  
+getItems(item);
+       
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+addItem(item);
+      
+removeItem(item);
+       
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+console.log(storage.getItems());
 storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+console.log(storage.getItems());
+
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// storage.addItem("Droid");
+//  console.log(storage.getItems());    
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems());
+
+
+
+
+
+
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
